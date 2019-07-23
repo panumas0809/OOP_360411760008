7223d14df0e0df32bc937992410825b95339eb79
kiw panumas <panumas08102541@gmail.com>
2019-07-23 10:40:44 +0700

    lab 2 ex1

---
+++ src/Lab2/ex1.java
@@ -0,0 +1,51 @@
+package Lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+        //data type in java
+        //integer
+        int x; // x is integer
+        x = 10;
+        System.out.println("x = "+x);
+        x = 20;
+        int a,b, c = 30;
+
+        //real number
+        double n = 20.5;
+        float m = 10;
+        System.out.println(n);
+        System.out.println(m);
+
+        //charecter
+        char d = '#';
+        System.out.println(d);
+
+        //String
+        String s = "panumas";
+        System.out.println(s);
+        String l = " chaichot";
+        String fullname = s + l;
+
+        System.out.println(fullname);
+        System.out.println(fullname.length());
+
+        String t = fullname.concat(" RUTS");
+        System.out.println(t);
+
+        //boolean
+        boolean w = true;
+        boolean o = false;
+        System.out.println(!w);
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+}//class
