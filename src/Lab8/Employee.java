43b071510e0b2299497d8bbbd3457fdd9adef5f4
kiw panumas <panumas08102541@gmail.com>
2019-09-17 11:39:31 +0700

    Employee&Person

---
+++ src/Lab8/Employee.java
@@ -0,0 +1,18 @@
+package Lab8;
+
+public class Employee extends Person {
+
+    private String empID;
+
+    public Employee(String name, String id,String empID) {
+        super(name, id);
+        this.empID = empID;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empID='" + empID + '\'' +
+                '}'+super.toString();
+    }
+}
---
+++ src/Lab8/EmployeeApp.java
@@ -0,0 +1,12 @@
+package Lab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("KIW","008","emp123");
+        System.out.println(emp1.toString());
+
+        Person p1 = new Employee("panumas","005","001");
+        System.out.println(p1.toString());
+
+    }
+}
